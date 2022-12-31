2eec6ae4ee1361f9b835405f8cb3d8cc1b0f2645
Myeongseon Kim <myeongseon.kim@unity3d.com>
2022-12-31 13:17:30 +0900

    CH16 - Instancing

--- FrameResource.cpp
+++ FrameResource.cpp
@@ -10,6 +10,18 @@ FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCo
     ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
 }
 
+#ifdef INSTANCING
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT maxInstanceCount, UINT materialCount)
+{
+    ThrowIfFailed(device->CreateCommandAllocator(
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        IID_PPV_ARGS(CmdListAlloc.GetAddressOf())));
+
+    PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
+    MaterialBuffer = std::make_unique<UploadBuffer<MaterialData>>(device, materialCount, false);
+    InstanceBuffer = std::make_unique<UploadBuffer<InstanceData>>(device, maxInstanceCount, false);
+}
+#else
 FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount)
 {
     ThrowIfFailed(device->CreateCommandAllocator(
@@ -22,6 +34,7 @@ FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCo
     MaterialBuffer = std::make_unique<UploadBuffer<MaterialData>>(device, materialCount, false);
     ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
 }
+#endif
 
 FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount, UINT waveVertCount)
 {
--- FrameResource.h
+++ FrameResource.h
@@ -4,6 +4,18 @@
 #include "../../Common/MathHelper.h"
 #include "../../Common/UploadBuffer.h"
 
+#define INSTANCING
+
+struct InstanceData
+{
+    DirectX::XMFLOAT4X4 World = MathHelper::Identity4x4();
+    DirectX::XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+    UINT MaterialIndex;
+    UINT InstancePad0;
+    UINT InstancePad1;
+    UINT InstancePad2;
+};
+
 struct ObjectConstants
 {
     DirectX::XMFLOAT4X4 World = MathHelper::Identity4x4();
@@ -83,8 +95,12 @@ struct FrameResource
 public:
 
     FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount);
-    FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount);
     FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount, UINT waveVertCount);
+#ifdef INSTANCING
+    FrameResource(ID3D12Device* device, UINT passCount, UINT maxInstanceCount, UINT materialCount);
+#else
+    FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount);
+#endif
     FrameResource(const FrameResource& rhs) = delete;
     FrameResource& operator=(const FrameResource& rhs) = delete;
     ~FrameResource();
@@ -105,6 +121,18 @@ public:
     std::unique_ptr<UploadBuffer<Vertex>> WavesVB = nullptr;
 
     std::unique_ptr<UploadBuffer<MaterialData>> MaterialBuffer = nullptr;
+
+#ifdef INSTANCING
+    // NOTE: In this demo, we instance only one render-item, so we only have one structured buffer to 
+    // store instancing data.  To make this more general (i.e., to support instancing multiple render-items), 
+    // you would need to have a structured buffer for each render-item, and allocate each buffer with enough
+    // room for the maximum number of instances you would ever draw.  
+    // This sounds like a lot, but it is actually no more than the amount of per-object constant data we 
+    // would need if we were not using instancing.  For example, if we were drawing 1000 objects without instancing,
+    // we would create a constant buffer with enough room for a 1000 objects.  With instancing, we would just
+    // create a structured buffer large enough to store the instance data for 1000 instances.  
+    std::unique_ptr<UploadBuffer<InstanceData>> InstanceBuffer = nullptr;
+#endif
 
     // Fence value to mark commands up to this fence point.  This lets us
     // check if these frame resources are still in use by the GPU.
